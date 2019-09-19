10032b9c2d462d610d05d0d5017b3df8458a6e0c
Gouri <gouri.josh@gmail.com>
2019-09-19 14:04:38 -0700

    created questions object

---
+++ assets/javascript/logic.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        question: "What was the first full length CGI movie?",
+        option1: "A Bug's Life",
+        option2: "Monsters Inc.",
+        option3: "Toy Story",
+        option4: "The Lion King",
+        answer: "Toy Story",
+        gif: ""
+    },
+    {
+        question: "Which of these is NOT a name of one of the Spice Girls?",
+        option1: "Sporty Spice",
+        option2: "Fred Spice",
+        option3: "Scary Spice",
+        option4: "Posh Spice",
+        answer: "Fred Spice",
+        gif: ""
+    },
+    {
+        question: "Which NBA team won the most titles in the 90s?",
+        option1: "New York Knicks",
+        option2: "Postland Trailblazers",
+        option3: "Los Angeles Lakers",
+        option4: "Chicago Bulls",
+        answer: "Chicago Bulls",
+        gif: ""
+    },
+    {
+        question: "Which group released the hit song, 'Smells Like Teen Spirit'?",
+        option1: "Nirvava",
+        option2: "Backstreet Boys",
+        option3: "The Offspring",
+        option4: "No Doubt",
+        answer: "Nirvava",
+        gif: ""
+    },   
+]
+
